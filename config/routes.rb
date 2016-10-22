e0ef305a441d397efe8b461be79152c828eb3d05
Clifton Regis <github email address>
2016-10-22 13:58:00 -0400

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'rooms#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount ActionCable.server => '/cable'
+  resource  :session
+
+  resources :videos do
+    resources :comments
+  end
+
 end
